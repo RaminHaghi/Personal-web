2a1e685edd54aee5a99df4a6fd0ed6dee3e0067e
Ramin Haghi <raminhaghi33@gmail.com>
2024-10-19 21:53:25 +0330

    finish header coding and styles

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,9 @@
 const navToggleBtn = document.querySelector('.menu-toggle')
 const menu = document.querySelector('.menu')
+const cover = document.querySelector('.cover')
 
 navToggleBtn.addEventListener('click', function() {
     this.classList.toggle('menu-toggle--open')
     menu.classList.toggle('menu--open')
+    cover.classList.toggle('cover--show')
 })
