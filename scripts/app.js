1112362366d7e0b77ab57ff87ca42a876da5490c
Ramin Haghi <raminhaghi33@gmail.com>
2024-10-19 21:20:37 +0330

    finish menu style and menu btn for mobile

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,7 @@
 const navToggleBtn = document.querySelector('.menu-toggle')
+const menu = document.querySelector('.menu')
 
 navToggleBtn.addEventListener('click', function() {
     this.classList.toggle('menu-toggle--open')
+    menu.classList.toggle('menu--open')
 })
