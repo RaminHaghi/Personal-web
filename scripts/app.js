df29af86a5cd7d54ff97b898033abc9dca51453d
Ramin Haghi <raminhaghi33@gmail.com>
2024-10-19 22:00:02 +0330

    refactor

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@ const navToggleBtn = document.querySelector('.menu-toggle')
 const menu = document.querySelector('.menu')
 const cover = document.querySelector('.cover')
 
-navToggleBtn.addEventListener('click', function() {
+navToggleBtn.addEventListener('click', function () {
     this.classList.toggle('menu-toggle--open')
     menu.classList.toggle('menu--open')
     cover.classList.toggle('cover--show')
