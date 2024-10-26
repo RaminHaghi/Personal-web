983d9b79b92c5da9fb9612c0500594af80dca07b
Ramin Haghi <raminhaghi33@gmail.com>
2024-10-26 22:50:49 +0330

    add javascript codes for resume list

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,16 @@
 const navToggleBtn = document.querySelector('.menu-toggle')
 const menu = document.querySelector('.menu')
 const cover = document.querySelector('.cover')
-
+const resumeListItems = document.querySelectorAll('.resume-list__item')
 navToggleBtn.addEventListener('click', function () {
     this.classList.toggle('menu-toggle--open')
     menu.classList.toggle('menu--open')
     cover.classList.toggle('cover--show')
+})
+
+resumeListItems.forEach(resumeListItem => {
+    resumeListItem.addEventListener('click', function(){
+        document.querySelector('.resume-list__item--active').classList.remove('resume-list__item--active')
+        this.classList.add('resume-list__item--active')
+    })
 })
